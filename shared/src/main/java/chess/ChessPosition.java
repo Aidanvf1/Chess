ee8fca6e0f3b159b6c66611c971a99953b50d424
Aidanvf1 <Aidanvf1@gmail.com>
2026-09-17 23:54:50 -0600

    Chess position

--- shared/src/main/java/chess/ChessPosition.java
+++ shared/src/main/java/chess/ChessPosition.java
@@ -1,4 +1,5 @@
 package chess;
+import java.util.Objects;
 
 public class ChessPosition {
     private final int row;
@@ -18,4 +19,17 @@ public class ChessPosition {
     public int getColumn() {
         return column;
     }
+    @Override
+    public boolean equals(Object o) {
+         if (this == o) return true;
+         if (o == null || getClass() != o.getClass()) return false;
+         ChessPosition that = (ChessPosition) o;
+         return (this.row == that.row) && (this.column == that.column);
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(row, column);
+    }
+
 }
